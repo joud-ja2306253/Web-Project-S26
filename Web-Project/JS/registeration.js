13572acdf3f1a0cd6e57a0e259da6d1950e7e3b3
sasabeel-sb2204518 <sb2204518@qu.edu.qa>
2026-03-25 06:23:18 +0300

    up

---
+++ Web-Project/JS/registeration.js
@@ -0,0 +1,27 @@
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const firstName = document.getElementById("fname").value.trim();
+  const lastName = document.getElementById("lname").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value;
+  const confirmPassword = document.getElementById("conf_password").value;
+
+  if (!firstName || !lastName || !email || !password || !confirmPassword) {
+    alert("Please fill all fields!");
+    return;
+  }
+
+  if (password !== confirmPassword) {
+    alert("Passwords do not match!");
+    return;
+  }
+
+  const user = {
+    firstName,
+    lastName,
+    email,
+    password
+  };
+
+  });
