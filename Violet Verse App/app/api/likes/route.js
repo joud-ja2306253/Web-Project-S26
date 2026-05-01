f05bd8959f7fc2edeae6c3055fed2906ac99d23a
shouq24 <sa2303945@qu.edu.qa>
2026-05-02 02:53:29 +0300

    push

--- Violet Verse App/app/api/likes/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { toggleLike } from "@/repo/InteractionRepository";
-
-export async function POST(req) {
-  const { userId, postId } = await req.json();
-  const result = await toggleLike(userId, postId);
-  return NextResponse.json(result);
-}
