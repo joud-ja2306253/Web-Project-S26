455162f8b11a7dcf069923d8d1b44f9ae00ab6fd
sasabeel-sb2204518 <sb2204518@qu.edu.qa>
2026-05-01 17:13:07 +0300

    i

---
+++ Violet Verse App/app/server/api/posts/[id]/comments/route.js
@@ -0,0 +1,43 @@
+import { NextResponse } from "next/server";
+import interactionRepo from "../../../../../repo/InteractionRepository";
+export async function GET(request, { params }) {
+  try {
+    // the post id 
+    const postId = Number(params.id);
+
+    const comments = await interactionRepo.getComments(postId);
+
+    return NextResponse.json(comments);
+
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
+
+export async function POST(request, { params }) {
+  try {
+    const postId = Number(params.id);
+
+    // reads the data 
+    const body = await request.json();
+
+    // in case there is no comment 
+    if (!body.comment || !body.comment.trim()) {
+      return NextResponse.json(
+        { error: "comment is required" },
+        { status: 400 }
+      );
+    }
+
+    const newComment = await interactionRepo.addComment({
+      comment: body.comment,
+      postId: postId,
+      userId: 1
+    });
+
+    return NextResponse.json(newComment, { status: 201 });
+
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
