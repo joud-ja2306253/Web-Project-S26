451361e1e799a9da27bc44b9c651d9b0ea8c0e9d
Joud-Pixels <ja2306253@qu.edu.qa>
2026-05-01 21:03:31 +0300

    add lib/prisma.js for a single instance of the data

---
+++ Violet Verse App/lib/prisma.js
@@ -0,0 +1,5 @@
+// lib/prisma.js 
+//reuse a single database connection across your entire app
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+export default prisma
