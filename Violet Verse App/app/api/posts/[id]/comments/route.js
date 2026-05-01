410fd63365ef05e28f0c55b2a314c3e4fdc556f7
shouq24 <sa2303945@qu.edu.qa>
2026-05-02 01:55:02 +0300

    .

--- Violet Verse App/app/api/posts/[id]/comments/route.js
+++ Violet Verse App/app/api/posts/[id]/comments/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import interactionRepo from "@/repos/InteractionRepository";
+import interactionRepo from "../../../../../repos/InteractionRepository";
 
 export async function GET(request, { params }) {
   try {
