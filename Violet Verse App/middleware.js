700ce72c130bab5aca5848d8bc6f25e1e6b36c2d
Joud-Pixels <ja2306253@qu.edu.qa>
2026-05-02 22:40:47 +0300

    Update middleware.js

--- Violet Verse App/middleware.js
+++ Violet Verse App/middleware.js
@@ -1,9 +1,20 @@
 import { NextResponse } from "next/server";
 
-export const runtime = 'nodejs';
+// config object
+export const config = {
+  runtime: 'nodejs',
+  matcher: [
+    "/",
+    "/login/:path*",
+    "/register/:path*",
+    "/profile/:path*",
+    "/create-post/:path*",
+    "/posts/:path*",
+  ],
+};
 
 // if logged in user tries to access /login or /register, redirect to home
-//if not logged in and tries to access protected routes, redirect to /login
+// if not logged in and tries to access protected routes, redirect to /login
 
 export function middleware(request) {
   const { pathname } = request.nextUrl;
@@ -24,14 +35,3 @@ export function middleware(request) {
 
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: [
-    "/",
-    "/login/:path*",
-    "/register/:path*",
-    "/profile/:path*",
-    "/create-post/:path*",
-    "/posts/:path*",
-  ],
-};
